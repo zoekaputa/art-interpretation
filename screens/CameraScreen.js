17dd2feb6565a87d2c7df778cf4e05c556bda26b
Zoe <zoe@zoekaputa.com>
2025-02-26 17:55:19 -0800

    wip add check artwork

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -7,6 +7,7 @@ import { useRef, useState } from "react";
 import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { FontAwesome6 } from "@expo/vector-icons";
 import theme from "../theme";
+import { containsArtwork } from "../scripts/gpt-request";
 
 /* This component is the Profile Screen */
 const CameraScreen = ({ route, navigation }) => {
@@ -33,6 +34,19 @@ const CameraScreen = ({ route, navigation }) => {
     );
   }
 
+  async function checkContainsArtwork() {
+    const contains = await containsArtwork();
+  }
+
+  useEffect(() => {
+    // TODO components a bit hacky
+    const startInference = () => {
+      const intervalId = setInterval(send_data, 30000); // 30 seconds
+    };
+
+    setTimeout(startInference, 5000); // 5 seconds
+  }, []);
+
   async function takePicture() {
     if (!isReady) {
       return;
@@ -57,14 +71,18 @@ const CameraScreen = ({ route, navigation }) => {
           facing={facing}
           flash={flash}
           accessibilityLabel="Camera View"
-          accessible={true} 
+          accessible={true}
           onCameraReady={() => setIsReady(true)}
         />
 
         {/* Camera Buttons Toolbar */}
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.centerButton} onPress={takePicture} accessibilityLabel="Take Picture"
-          accessible={true} >
+          <TouchableOpacity
+            style={styles.centerButton}
+            onPress={takePicture}
+            accessibilityLabel="Take Picture"
+            accessible={true}
+          >
             <FontAwesome6 name="circle" size={75} color="white" solid />
           </TouchableOpacity>
         </View>
@@ -90,15 +108,15 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     color: theme.colors.darkBlue,
     textShadowColor: theme.colors.lightBlue,
-    textShadowOffset: { width: 1, height: 1 }, 
-    textShadowRadius: 3, 
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 3,
   },
   cameraWrapper: {
-    width: "100%", 
+    width: "100%",
     height: "80%",
     borderRadius: 10,
     overflow: "hidden",
-    boxShadow: '0px 0px 10px 4px rgba(141, 193, 221, 0.50)',
+    boxShadow: "0px 0px 10px 4px rgba(141, 193, 221, 0.50)",
   },
   camera: {
     flex: 1,
@@ -106,7 +124,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     position: "absolute",
     bottom: 0,
-    width: "100%", 
+    width: "100%",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
@@ -126,7 +144,7 @@ const styles = StyleSheet.create({
     margin: "auto",
   },
   textContainer: {
-    width: 50,  
+    width: 50,
     height: 50,
     justifyContent: "center",
     alignItems: "center",
